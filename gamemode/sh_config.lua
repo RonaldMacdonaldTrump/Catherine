37a1bfaef7d62b4bc8650cb547941c5104ce6fec
L7D <smhjyh2009@gmail.com>
2015-02-02 20:11:46 +0900

    lol
    L7D - :)

--- gamemode/sh_config.lua
+++ gamemode/sh_config.lua
@@ -9,6 +9,6 @@ nexus.configs.playerDefaultWalkSpeed = 200
 nexus.configs.database_module = "mysqloo"
 nexus.configs.database_host = "127.0.0.1"
 nexus.configs.database_id = "root"
-nexus.configs.database_pwd = "DGKaAnrKtG2jMLBV"
+nexus.configs.database_pwd = ""
 nexus.configs.database_name = "nexus"
 nexus.configs.database_port = 3306
