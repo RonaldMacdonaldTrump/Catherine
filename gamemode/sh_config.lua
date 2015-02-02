c07489ca9dfbd2d759b414c9515253495de1e874
Fristet <guseh7@gmail.com>
2015-02-02 20:43:02 +0900

    config setting

--- gamemode/sh_config.lua
+++ gamemode/sh_config.lua
@@ -4,11 +4,15 @@ nexus.configs = nexus.configs or { }
 
 nexus.configs.doorCost = 50
 nexus.configs.doorSellCost = 25
+
 nexus.configs.playerDefaultRunSpeed = 350
 nexus.configs.playerDefaultWalkSpeed = 200
+nexus.configs.playerDefaultWalkSpeed = 200
+nexus.configs.playerJumpPower = 130
 
 nexus.configs.defaultMoney = 100
 nexus.configs.moneyName = "Token"
+nexus.configs.moneyModel = "models/props_lab/box01a.mdl"
 
 nexus.configs.database_module = "mysqloo"
 nexus.configs.database_host = "127.0.0.1"
