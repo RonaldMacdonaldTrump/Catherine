5f98a10365b359821af6c63b1345b799f6ac193f
L7D <smhjyh2009@gmail.com>
2015-02-03 21:07:41 +0900

    Door Fix/Cash
    L7D - :)

--- entities/entities/nexus_money.lua
+++ entities/entities/nexus_money.lua
@@ -1,15 +1,19 @@
-
 AddCSLuaFile( )
 
+DEFINE_BASECLASS( "base_gmodentity" )
+
+ENT.PrintName = "Nexus Money"
+ENT.Author = "L7D, Fristet"
 ENT.Type = "anim"
 
 function ENT:Initialize( )
 	if ( CLIENT ) then return end
 	
-	self:SetModel( nexus.configs.moneyModel )
+	self:SetModel( nexus.configs.cashModel )
 	self:PhysicsInit( SOLID_VPHYSICS )
 	self:SetMoveType( MOVETYPE_VPHYSICS )
 	self:SetSolid( SOLID_VPHYSICS )
+	self:SetHealth( 20 )
 	
 	self:SetUseType( SIMPLE_USE )
 	
@@ -17,10 +21,40 @@ function ENT:Initialize( )
 	phys:Wake( )
 end
 
+function ENT:SetCash( amount )
+	if ( amount == 0 ) then
+		self:Remove( )
+		return
+	end
+	self:SetNetworkValue( "Cash", amount )
+end
+--[[
+function ENT:GetCash( )
+	return self:GetNetworkValue( "Cash", 0 )
+end
+--]]
 function ENT:Use( activator )
-	local amt = 100
-
-	GAMEMODE:Notify(activator, 2, 5, "You got a " .. amt .. nexus.configs.moneyName .. "s!" )
-
+	activator:GiveCash( self:GetNetworkValue( "Cash", 0 ) )
+	activator:ChatPrint( "You got a " .. nexus.cash.GetName( self:GetNetworkValue( "Cash", 0 ) ) .. "s!" )
+	self:Bomb( )
 	self:Remove( )
+end
+
+function ENT:Bomb( )
+	local eff = EffectData( )
+	eff:SetStart( self:GetPos( ) )
+	eff:SetOrigin( self:GetPos( ) )
+	eff:SetScale( 8 )
+	util.Effect( "GlassImpact", eff, true, true )
+	
+	self:EmitSound( "physics/body/body_medium_impact_soft" .. math.random( 1, 7 ) .. ".wav" )
+end
+
+function ENT:OnTakeDamage( dmg )
+	self:SetHealth( math.max( self:Health( ) - dmg:GetDamage( ), 0 ) )
+	
+	if ( self:Health( ) <= 0 ) then
+		self:Bomb( )
+		self:Remove( )
+	end
 end
--- gamemode/libs/sh_door.lua
+++ gamemode/libs/sh_door.lua
@@ -1,42 +1,36 @@
 local META = FindMetaTable( "Player" )
 
 if ( SERVER ) then
-
-local ent = ply:GetEyeTrace(70).Entity
-
-function META:BuyDoor( )
-
-	if self:GetCash( ) => nexus.configs.doorCost or ent:IsDoor( ) or ent:GetOwner( ) == nil then
-		self:ChatPrint( "You has been bought the door!" )
-		ent:SetOwner( self:GetCharacterID( ) )
-		self:TakeCash( nexus.configs.doorCost )
-	elseif self:GetCahs( ) =< nexus.configs.doorCost then
-		self:ChatPrint( "You need" ..  ( nexus.configs.doorCost  - self:GetCash ) .. nexus.configs.cashName .. "(s) more!" )
+	function META:BuyDoor( )
+		local ent = self:GetEyeTrace( 70 ).Entity
+		if ( self:GetCash( ) >= nexus.configs.doorCost or ent:IsDoor( ) or ent:GetOwner( ) == nil ) then
+			self:ChatPrint( "You has been bought the door!" )
+			ent:SetOwner( self:GetCharacterID( ) )
+			self:TakeCash( nexus.configs.doorCost )
+		elseif ( self:GetCash( ) <= nexus.configs.doorCost ) then
+			self:ChatPrint( "You need" .. ( nexus.configs.doorCost - self:GetCash( ) ) .. nexus.configs.cashName .. "(s) more!" )
+		end
 	end
 
-end
-
-function META:SellDoor( )
-
-	if ent:IsDoor( ) or ent:GetOwner( ) == self:GetCharacterID( ) then
-		self:ChatPrint( "You has been sold the door!" )
-		self:GiveCash( nexus.configs.doorSellCost )
-		ent:SetOwner( nil )
-	else
-		self:ChatPrint( "You don't have a permission!" )
-	end
-end
-
-function META:SetDoorTitle( args )
-
-	if ent:IsDoor( ) or ent:GetOwner( ) == self:GetCharacterID( ) then
-		ent:SetNWInt( "title", args  )
+	function META:SellDoor( )
+		local ent = self:GetEyeTrace( 70 ).Entity
+		if ( ent:IsDoor( ) or ent:GetOwner( ) == self:GetCharacterID( ) ) then
+			self:ChatPrint( "You has been sold the door!" )
+			self:GiveCash( nexus.configs.doorSellCost )
+			ent:SetOwner( nil )
+		else
+			self:ChatPrint( "You don't have a permission!" )
+		end
 	end
 
-end
+	function META:SetDoorTitle( args )
+		local ent = self:GetEyeTrace( 70 ).Entity
+		if ( ent:IsDoor( ) or ent:GetOwner( ) == self:GetCharacterID( ) ) then
+			ent:SetNetworkValue( "Title", args )
+		end
+	end
 
-concommand.Add( "DoorSetTitle", function( cmd, args ) 
-	ent:SetDoorTitle( tonumber(args[1]) )
-end
-
+	concommand.Add( "DoorSetTitle", function( cmd, args ) 
+		ent:SetDoorTitle( tostring( args[ 1 ] ) )
+	end )
 end
