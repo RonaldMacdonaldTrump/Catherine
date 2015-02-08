c81b05843394109ee184f3e30b58236b462d5021
L7D <smhjyh2009@gmail.com>
2015-02-08 13:39:43 +0900

    Catherine data bug fix
    L7D - :)

--- gamemode/libs/sh_data.lua
+++ gamemode/libs/sh_data.lua
@@ -7,7 +7,6 @@ if ( SERVER ) then
 		if ( !catherine.catherine_data.buffers[ pl:SteamID( ) ] ) then return end
 		catherine.catherine_data.SaveToMySQL( pl )
 		catherine.catherine_data.buffers[ pl:SteamID( ) ] = nil
-		print("Init!")
 		// Init z;
 	end )
 	
@@ -20,11 +19,17 @@ if ( SERVER ) then
 			if ( #data == 0 ) then return end
 			local data = data[ 1 ][ "_catherineData" ]
 			catherine.catherine_data.buffers[ pl:SteamID( ) ] = util.JSONToTable( data )
-			print("Transfer finished")
+			catherine.catherine_data.SendToPlayer( pl )
 			if ( isLoading ) then
 				netstream.Start( pl, "catherine.LoadingStatus", "Registering catherine data finished ..." )
 			end
 		end )
+	end
+	
+	function catherine.catherine_data.SendToPlayer( pl )
+		if ( !IsValid( pl ) ) then return end
+		if ( !catherine.catherine_data.buffers[ pl:SteamID( ) ] ) then return end
+		netstream.Start( pl, "catherine.catherine_data.SendcatherineData", catherine.catherine_data.buffers[ pl:SteamID( ) ] )
 	end
 
 	function catherine.catherine_data.SaveToMySQL( pl )
@@ -33,9 +38,7 @@ if ( SERVER ) then
 		local data = {
 			_catherineData = util.TableToJSON( catherine.catherine_data.buffers[ pl:SteamID( ) ] )
 		}
-		catherine.database.Update( "_steamID = '" .. pl:SteamID( ) .. "'", data, "catherine_players", function( )
-			print("Save finished")
-		end )
+		catherine.database.Update( "_steamID = '" .. pl:SteamID( ) .. "'", data, "catherine_players" )
 	end
 	
 	function catherine.catherine_data.SetcatherineData( pl, key, value, nosync, save )
@@ -49,11 +52,6 @@ if ( SERVER ) then
 			catherine.catherine_data.SaveToMySQL( pl )
 		end
 	end
---[[
-	catherine.catherine_data.SetcatherineData( player.GetByID( 1 ), "test", nil )
-	catherine.catherine_data.SetcatherineData( player.GetByID( 1 ), "zz", nil )
-	catherine.catherine_data.SetcatherineData( player.GetByID( 1 ), "gg", nil )--]]
-	//catherine.catherine_data.SaveToMySQL( player.GetByID( 1 ) )
 
 	function catherine.catherine_data.GetcatherineData( pl, key, default )
 		if ( !IsValid( pl ) or !key ) then return default end
@@ -73,5 +71,8 @@ else
 		catherine.catherine_data.localData = catherine.catherine_data.localData or { }
 		catherine.catherine_data.localData[ data[ 1 ] ] = data[ 2 ]
 	end )
+	
+	netstream.Hook( "catherine.catherine_data.SendcatherineData", function( data )
+		catherine.catherine_data.localData = data
+	end )
 end
-
