a511184f3b07abce9672a4801e5772fd45a540cf
L7D <smhjyh2009@gmail.com>
2015-02-08 20:26:58 +0900

    Loading screen fix
    L7D - :)

--- gamemode/hooks/cl_hooks.lua
+++ gamemode/hooks/cl_hooks.lua
@@ -1,9 +1,12 @@
 
 catherine.loading = catherine.loading or false
-catherine.loadingText = catherine.loadingText or ""
-catherine.loadingAlpha = catherine.loadingAlpha or 255
-catherine.loadingImageRotated = catherine.loadingImageRotated or 0
-catherine.loadingImageAlpha = catherine.loadingImageAlpha or 255
+catherine.errorText = catherine.errorText or ""
+catherine.alpha = catherine.alpha or 255
+catherine.color = catherine.color or 0
+catherine.textColor = catherine.textColor or 255
+catherine.loadingStarting = catherine.loadingStarting or false
+catherine.progressBar = catherine.progressBar or 0
+catherine.percent = catherine.percent or 0
 
 
 catherine.menuList = catherine.menuList or { }
@@ -24,32 +27,39 @@ function GM:HUDShouldDraw( element )
 	return true
 end
 
+
 function GM:HUDDrawScoreBoard( )
 	local scrW, scrH = ScrW( ), ScrH( )
 	if ( catherine.loading ) then
-		catherine.loadingAlpha = Lerp( 0.01, catherine.loadingAlpha, 255 )
+		if ( catherine.loadingStarting ) then
+			catherine.alpha = Lerp( 0.01, catherine.alpha, 255 )
+			catherine.color = Lerp( 0.01, catherine.color, 0 )
+			catherine.textColor = Lerp( 0.01, catherine.textColor, 255 )
+		else
+			catherine.color = Lerp( 0.01, catherine.color, 255 )
+			catherine.alpha = Lerp( 0.01, catherine.alpha, 255 )
+			catherine.textColor = Lerp( 0.01, catherine.textColor, 50 )
+		end
 	else
-		catherine.loadingAlpha = Lerp( 0.005, catherine.loadingAlpha, 0 )
-		catherine.loadingImageAlpha = Lerp( 0.03, catherine.loadingImageAlpha, 0 )
-		catherine.loadingImageRotated = Lerp( 0.03, catherine.loadingImageRotated, 0 )
+		catherine.alpha = Lerp( 0.005, catherine.alpha, 0 )
 	end
 	
-	if ( math.Round( catherine.loadingImageRotated ) <= 359 ) then
-		catherine.loadingImageRotated = Lerp( 0.01, catherine.loadingImageRotated, 360 )
-	else
-		catherine.loadingImageRotated = 0
-	end
+	catherine.progressBar = Lerp( 0.05, catherine.progressBar, ( scrW - 20 ) * catherine.percent )
+
+	draw.RoundedBox( 0, 0, 0, scrW, scrH, Color( catherine.color, catherine.color, catherine.color, catherine.alpha ) )
 	
-	draw.RoundedBox( 0, 0, 0, scrW, scrH, Color( 40, 40, 40, catherine.loadingAlpha ) )
-	surface.SetDrawColor( 0, 0, 0, catherine.loadingAlpha )
+	surface.SetDrawColor( catherine.color - 55, catherine.color - 55, catherine.color - 55, catherine.alpha )
 	surface.SetMaterial( Material( "gui/gradient_up" ) )
 	surface.DrawTexturedRect( 0, 0, scrW, scrH )
-	--[[
-	surface.SetDrawColor( 255, 255, 255, catherine.loadingImageAlpha )
-	surface.SetMaterial( Material( "catherine/loading2.png" ) )
-	surface.DrawTexturedRectRotated( scrW / 2, scrH / 2, 184, 184, catherine.loadingImageRotated )
-	--]]
-	draw.SimpleText( catherine.loadingText, "catherine_font01_25", scrW / 2, scrH - 70, Color( 255, 255, 255, catherine.loadingAlpha ), 1, 1 )
+
+	surface.SetDrawColor( catherine.textColor, catherine.textColor, catherine.textColor, catherine.alpha )
+	surface.SetMaterial( Material( "catherine/logo.png" ) )
+	surface.DrawTexturedRect( scrW / 2 - 512 / 2, scrH / 2 - 256 / 2, 512, 256 )
+	
+	draw.RoundedBox( 0, 10, scrH - 20, scrW - 20, 10, Color( catherine.textColor * 2, catherine.textColor * 2, catherine.textColor * 2, catherine.alpha ) )
+	draw.RoundedBox( 0, 10, scrH - 20, catherine.progressBar, 10, Color( catherine.textColor, catherine.textColor, catherine.textColor, catherine.alpha ) )
+	
+	draw.SimpleText( catherine.errorText, "catherine_font01_40", scrW / 2, scrH - 70, Color( catherine.textColor, catherine.textColor, catherine.textColor, catherine.alpha ), 1, 1 )
 end
 
 function GM:AddMenu( )
@@ -57,11 +67,21 @@ function GM:AddMenu( )
 end
 
 netstream.Hook( "catherine.LoadingStatus", function( data )
-	if ( type( data ) == "boolean" ) then
-		catherine.loading = data
-		catherine.loadingAlpha = 255
-		catherine.loadingImageAlpha = 255
-	else
-		catherine.loadingText = data
+	if ( data[ 2 ] == true ) then
+		if ( data[ 1 ] == true ) then
+			catherine.loading = true
+			catherine.loadingStarting = false
+		elseif ( data[ 1 ] == false ) then
+			catherine.loading = true
+			catherine.loadingStarting = true
+		end
+	elseif ( data[ 2 ] == false ) then
+		catherine.loading = true
+		catherine.errorText = data[ 3 ] or ""
+	end
+	catherine.percent = data[ 4 ]
+	if ( data[ 5 ] == true ) then
+		catherine.loading = false
+		catherine.loadingStarting = false
 	end
 end )
--- gamemode/hooks/sv_hooks.lua
+++ gamemode/hooks/sv_hooks.lua
@@ -16,47 +16,74 @@ function GM:PlayerSpawn( pl )
 	player_manager.SetPlayerClass( pl, "catherine_player" )
 	BaseClass.PlayerSpawn( self, pl )
 end
+--[[
+netstream.Hook( "catherine.LoadingStatus", function( data )
+	if ( data[ 2 ] == true ) then
+		if ( data[ 1 ] == true ) then
+			catherine.loading = true
+			catherine.loadingStarting = false
+			catherine.progressBar = 0
+		elseif ( data[ 1 ] == false ) then
+			catherine.loading = true
+			catherine.loadingStarting = true
+			catherine.progressBar = 0
+		end
+	elseif ( data[ 2 ] == false ) then
+		catherine.loading = true
+		catherine.progressBar = 0
+		catherine.errorText = data[ 3 ] or ""
+	end
+	catherine.percent = data[ 4 ]
+	if ( data[ 5 ] == true ) then
+		catherine.loading = false
+		catherine.loadingStarting = false
+	end
+end )
+--]]
 
 function GM:PlayerInitialSpawn( pl )
 	pl:SetNoDraw( true )
 	pl:KillSilent( )
-		local function stap01( )
-		netstream.Start( pl, "catherine.LoadingStatus", "Character networking ..." )
+	local function stap01( )
+		if ( !catherine.database.Connected ) then
+			netstream.Start( pl, "catherine.LoadingStatus", { false, false, "Catherine has not connected by MySQL!", 0 } )
+			return
+		end
+		netstream.Start( pl, "catherine.LoadingStatus", { true, true, 1, 0.15 } )
 		
 		catherine.character.SendCurrentCharacterDatas( pl )
 		
+		netstream.Start( pl, "catherine.LoadingStatus", { true, true, 1, 0.3 } )
+		
 		catherine.database.GetTable( "_steamID = '" .. pl:SteamID( ) .. "'", "catherine_players", function( data )
 			if ( #data == 0 ) then
-				netstream.Start( pl, "catherine.LoadingStatus", "Makeing player tables ..." )
-				print("Makeing player tables ..." )
+				netstream.Start( pl, "catherine.LoadingStatus", { true, true, 1, 0.4 } )
 				catherine.database.Insert( {
 					_steamName = pl:Name( ),
 					_steamID = pl:SteamID( ),
 					_catherineData = "[]"
 				}, "catherine_players", function( )
-					netstream.Start( pl, "catherine.LoadingStatus", "Maked player tables ..." )
-					catherine.catherine_data.RegisterByMySQL( pl, true )
-					print("Register catherine data ..." )
+					netstream.Start( pl, "catherine.LoadingStatus", { true, true, 1, 0.5 } )
+					catherine.catherine_data.RegisterByMySQL( pl )
+					netstream.Start( pl, "catherine.LoadingStatus", { true, true, 1, 1 } )
 					timer.Simple( 3, function( )
-						netstream.Start( pl, "catherine.LoadingStatus", "Loaded!" )
-						netstream.Start( pl, "catherine.LoadingStatus", false )
 						catherine.character.SendCharacterLists( pl )
 						catherine.character.SendCharacterPanel( pl )
+						netstream.Start( pl, "catherine.LoadingStatus", { 1, 1, 1, 1, true } )
 					end )
-					print("Maked player tables ..." )
 				end )
 			else
-				catherine.catherine_data.RegisterByMySQL( pl, true )
-				print("Register catherine data ..." )
+				catherine.catherine_data.RegisterByMySQL( pl )
+				netstream.Start( pl, "catherine.LoadingStatus", { true, true, 1, 1 } )
 				timer.Simple( 3, function( )
-					netstream.Start( pl, "catherine.LoadingStatus", "Loaded!" )
-					netstream.Start( pl, "catherine.LoadingStatus", false )
 					catherine.character.SendCharacterLists( pl )
 					catherine.character.SendCharacterPanel( pl )
+					netstream.Start( pl, "catherine.LoadingStatus", { 1, 1, 1, 1, true } )
 				end )
 			end
 		end )
 	end
+
 	
 	timer.Create( "catherine.loading.WaitLocalPlayer_" .. pl:SteamID( ), 1, 0, function( )
 		//netstream.Start( pl, "catherine.LoadingStatus", "Waiting local player ..." )
@@ -64,57 +91,57 @@ function GM:PlayerInitialSpawn( pl )
 		if ( IsValid( pl ) ) then
 			print("Start")
 			timer.Destroy( "catherine.loading.WaitLocalPlayer_" .. pl:SteamID( ) )
-			netstream.Start( pl, "catherine.LoadingStatus", true )
-			
-			netstream.Start( pl, "catherine.LoadingStatus", "Waiting local player ..." )
-			timer.Simple( 3, function( )
+			netstream.Start( pl, "catherine.LoadingStatus", { false, true, 1, 0.1 } )
+			timer.Simple( 7, function( )
 				stap01( )
 			end )
 		end
 	end )
-	
 end
 
-concommand.Add( "panelTest", function( pl )
-
-	catherine.character.SendCharacterPanel( pl )
-end )
-
 concommand.Add( "loadingTest", function( pl )
-	
+	pl:SetNoDraw( true )
+	pl:KillSilent( )
 	local function stap01( )
-		netstream.Start( pl, "catherine.LoadingStatus", "Character networking ..." )
+		if ( !catherine.database.Connected ) then
+			netstream.Start( pl, "catherine.LoadingStatus", { false, false, "Catherine has not connected by MySQL!", 0 } )
+			return
+		end
+		netstream.Start( pl, "catherine.LoadingStatus", { true, true, 1, 0.15 } )
 		
 		catherine.character.SendCurrentCharacterDatas( pl )
 		
+		netstream.Start( pl, "catherine.LoadingStatus", { true, true, 1, 0.3 } )
+		
 		catherine.database.GetTable( "_steamID = '" .. pl:SteamID( ) .. "'", "catherine_players", function( data )
 			if ( #data == 0 ) then
-				netstream.Start( pl, "catherine.LoadingStatus", "Makeing player tables ..." )
-				print("Makeing player tables ..." )
+				netstream.Start( pl, "catherine.LoadingStatus", { true, true, 1, 0.4 } )
 				catherine.database.Insert( {
 					_steamName = pl:Name( ),
 					_steamID = pl:SteamID( ),
 					_catherineData = "[]"
 				}, "catherine_players", function( )
-					netstream.Start( pl, "catherine.LoadingStatus", "Maked player tables ..." )
-					catherine.catherine_data.RegisterByMySQL( pl, true )
-					print("Register catherine data ..." )
+					netstream.Start( pl, "catherine.LoadingStatus", { true, true, 1, 0.5 } )
+					catherine.catherine_data.RegisterByMySQL( pl )
+					netstream.Start( pl, "catherine.LoadingStatus", { true, true, 1, 1 } )
 					timer.Simple( 3, function( )
-						netstream.Start( pl, "catherine.LoadingStatus", "Loaded!" )
-						netstream.Start( pl, "catherine.LoadingStatus", false )
+						catherine.character.SendCharacterLists( pl )
+						catherine.character.SendCharacterPanel( pl )
+						netstream.Start( pl, "catherine.LoadingStatus", { 1, 1, 1, 1, true } )
 					end )
-					print("Maked player tables ..." )
 				end )
 			else
-				catherine.catherine_data.RegisterByMySQL( pl, true )
-				print("Register catherine data ..." )
+				catherine.catherine_data.RegisterByMySQL( pl )
+				netstream.Start( pl, "catherine.LoadingStatus", { true, true, 1, 1 } )
 				timer.Simple( 3, function( )
-					netstream.Start( pl, "catherine.LoadingStatus", "Loaded!" )
-					netstream.Start( pl, "catherine.LoadingStatus", false )
+					catherine.character.SendCharacterLists( pl )
+					catherine.character.SendCharacterPanel( pl )
+					netstream.Start( pl, "catherine.LoadingStatus", { 1, 1, 1, 1, true } )
 				end )
 			end
 		end )
 	end
+
 	
 	timer.Create( "catherine.loading.WaitLocalPlayer_" .. pl:SteamID( ), 1, 0, function( )
 		//netstream.Start( pl, "catherine.LoadingStatus", "Waiting local player ..." )
@@ -122,15 +149,12 @@ concommand.Add( "loadingTest", function( pl )
 		if ( IsValid( pl ) ) then
 			print("Start")
 			timer.Destroy( "catherine.loading.WaitLocalPlayer_" .. pl:SteamID( ) )
-			netstream.Start( pl, "catherine.LoadingStatus", true )
-			
-			netstream.Start( pl, "catherine.LoadingStatus", "Waiting local player ..." )
-			timer.Simple( 3, function( )
+			netstream.Start( pl, "catherine.LoadingStatus", { false, true, 1, 0.1 } )
+			timer.Simple( 7, function( )
 				stap01( )
 			end )
 		end
 	end )
-
 end )
 
 function GM:PlayerHealthChanged( pl )
