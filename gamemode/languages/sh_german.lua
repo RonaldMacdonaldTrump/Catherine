65b8b611e3d5f51a43a80f758592744df86195dd
Tomelyr <Tomelyr@googlemail.com>
2015-03-23 16:23:31 +0100

    German Lang

---
+++ gamemode/languages/sh_german.lua
@@ -0,0 +1,27 @@
+Lang.name = "German"
+Lang.datas = {
+	[ "test" ] = "Dies ist die Deutsche Sprachdatei.",
+	[ "test2" ] = "Dies ist die %s Sprachdatei.",
+	
+	
+	
+	
+	
+	
+	
+	// Cash ^-^;
+	[ "Cash_GiveMessage01" ] = "Du hast %s an %s überwiesen.",
+	
+	// Faction ^-^;
+	[ "Faction_AddMessage01" ] = "Setze die Fraktion",
+	[ "Faction_RemoveMessage01" ] = "Entferne die Fraktion",
+	
+	// Flag ^-^;
+	[ "Flag_GiveMessage01" ] = "Füge flag hinzu",
+	[ "Flag_TakeMessage01" ] = "Entferne flag",
+	
+	[ "UnknownError" ] = "Unbekannter Fehler.",
+	[ "UnknownPlayerError" ] = "Der eingegebende Name ist ungültig!",
+	[ "ArgError" ] = "%s Bitte füge ein zweites Argument hinzu!"
+	
+}
