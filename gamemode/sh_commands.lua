2456c4b7edc14f3f30df27584065519c38f34c9a
L7D <smhjyh2009@gmail.com>
2015-04-03 16:44:27 +0900

    Fix

--- gamemode/sh_commands.lua
+++ gamemode/sh_commands.lua
@@ -90,6 +90,44 @@ catherine.command.Register( {
 } )
 
 catherine.command.Register( {
+	command = "charsetmodel",
+	canRun = function( pl ) return pl:IsAdmin( ) end,
+	runFunc = function( pl, args )
+		if ( args[ 1 ] ) then
+			if ( args[ 2 ] ) then
+				local target = catherine.util.FindPlayerByName( args[ 1 ] )
+				if ( IsValid( target ) and target:IsPlayer( ) ) then
+					catherine.character.SetGlobalVar( target, "_model", args[ 2 ] )
+					catherine.util.Notify( pl, "Set model" )
+				else
+					catherine.util.Notify( pl, catherine.language.GetValue( pl, "UnknownPlayerError" ) )
+				end
+			else
+				catherine.util.Notify( pl, catherine.language.GetValue( pl, "ArgError", 2 ) )
+			end
+		else
+			catherine.util.Notify( pl, catherine.language.GetValue( pl, "ArgError", 1 ) )
+		end
+	end
+} )
+
+catherine.command.Register( {
+	command = "charphysdesc",
+	runFunc = function( pl, args )
+		if ( args[ 1 ] ) then
+			if ( args[ 1 ]:len( ) >= catherine.configs.characterDescMinLen and args[ 1 ]:len( ) < catherine.configs.characterDescMaxLen ) then
+				catherine.character.SetGlobalVar( target, "_desc", args[ 1 ] )
+				catherine.util.Notify( pl, "Set your desc" )
+			else
+				catherine.util.Notify( pl, "The character description must be at least " .. catherine.configs.characterDescMinLen .." characters long and up to " .. catherine.configs.characterDescMaxLen .. " characters!" )
+			end
+		else
+			catherine.util.Notify( pl, catherine.language.GetValue( pl, "ArgError", 1 ) )
+		end
+	end
+} )
+
+catherine.command.Register( {
 	command = "doorlock",
 	canRun = function( pl ) return pl:IsAdmin( ) end,
 	runFunc = function( pl, args )
