bca170979ef46d55c0dbc7628b9239dd694e8fc2
L7D <smhjyh2009@gmail.com>
2015-04-03 16:36:39 +0900

    Door stuff.

--- gamemode/sh_commands.lua
+++ gamemode/sh_commands.lua
@@ -66,3 +66,57 @@ catherine.command.Register( {
 		end
 	end
 } )
+
+catherine.command.Register( {
+	command = "charsetdesc",
+	canRun = function( pl ) return pl:IsAdmin( ) end,
+	runFunc = function( pl, args )
+		if ( args[ 1 ] ) then
+			if ( args[ 2 ] ) then
+				local target = catherine.util.FindPlayerByName( args[ 1 ] )
+				if ( IsValid( target ) and target:IsPlayer( ) ) then
+					catherine.character.SetGlobalVar( target, "_desc", args[ 2 ] )
+					catherine.util.Notify( pl, "Set desc" )
+				else
+					catherine.util.Notify( pl, catherine.language.GetValue( pl, "UnknownPlayerError" ) )
+				end
+			else
+				catherine.util.Notify( pl, catherine.language.GetValue( pl, "ArgError", 2 ) )
+			end
+		else
+			catherine.util.Notify( pl, catherine.language.GetValue( pl, "ArgError", 1 ) )
+		end
+	end
+} )
+
+catherine.command.Register( {
+	command = "doorlock",
+	canRun = function( pl ) return pl:IsAdmin( ) end,
+	runFunc = function( pl, args )
+		local ent = pl:GetEyeTraceNoCursor( ).Entity
+		
+		if ( IsValid( ent ) and ent:IsDoor( ) ) then
+			ent:Fire( "Lock", "", 0 ) 
+			ent:EmitSound( "doors/door_latch3.wav" )
+			catherine.util.Notify( pl, "You are locked this door." )
+		else
+			catherine.util.Notify( pl, "This is not a valid door!" )
+		end
+	end
+} )
+
+catherine.command.Register( {
+	command = "doorunlock",
+	canRun = function( pl ) return pl:IsAdmin( ) end,
+	runFunc = function( pl, args )
+		local ent = pl:GetEyeTraceNoCursor( ).Entity
+		
+		if ( IsValid( ent ) and ent:IsDoor( ) ) then
+			ent:Fire( "Unlock", "", 0 ) 
+			ent:EmitSound( "doors/door_latch3.wav" )
+			catherine.util.Notify( pl, "You are unlocked this door." )
+		else
+			catherine.util.Notify( pl, "This is not a valid door!" )
+		end
+	end
+} )
