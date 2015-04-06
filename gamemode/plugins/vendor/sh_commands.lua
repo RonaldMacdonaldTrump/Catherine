5ff5e5072562a40be1b4cd05198077ebe692eee0
L7D <smhjyh2009@gmail.com>
2015-04-06 14:30:16 +0900

    Vendor base / Plugin system optimize.

---
+++ gamemode/plugins/vendor/sh_commands.lua
@@ -0,0 +1,48 @@
+--[[
+< CATHERINE > - A free role-playing framework for Garry's Mod.
+Development and design by L7D.
+
+Catherine is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Catherine.  If not, see <http://www.gnu.org/licenses/>.
+]]--
+
+local PLUGIN = PLUGIN
+
+catherine.command.Register( {
+	command = "textadd",
+	syntax = "[Text] [Size]",
+	canRun = function( pl ) return pl:IsAdmin( ) end,
+	runFunc = function( pl, args )
+		if ( args[ 1 ] ) then
+			PLUGIN:AddText( pl, args[ 1 ], tonumber( args[ 2 ] ) )
+			catherine.util.Notify( pl, "You have added text to your desired location!" )
+		else
+			catherine.util.Notify( pl, "args[ 1 ] is missing!" )
+		end
+	end
+} )
+
+catherine.command.Register( {
+	command = "textremove",
+	syntax = "[Distance]",
+	canRun = function( pl ) return pl:IsAdmin( ) end,
+	runFunc = function( pl, args )
+		if ( !args[ 1 ] ) then args[ 1 ] = 256 end
+		local count = PLUGIN:RemoveText( pl:GetShootPos( ), args[ 1 ] )
+		if ( count == 0 ) then
+			catherine.util.Notify( pl, "There are no texts at that location." )
+		else
+			catherine.util.Notify( pl, "You have removed " .. count .. "'s texts!" )
+		end
+	end
+} )
---
+++ gamemode/plugins/vendor/sv_plugin.lua
@@ -0,0 +1,81 @@
+--[[
+< CATHERINE > - A free role-playing framework for Garry's Mod.
+Development and design by L7D.
+
+Catherine is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Catherine.  If not, see <http://www.gnu.org/licenses/>.
+]]--
+
+local PLUGIN = PLUGIN
+
+function PLUGIN:EntityDataLoaded( pl )
+	self:SyncTextAll( pl )
+end
+
+function PLUGIN:SaveTexts( )
+	catherine.data.Set( "goodtexts", self.textLists )
+end
+
+function PLUGIN:LoadTexts( )
+	self.textLists = catherine.data.Get( "goodtexts", { } )
+end
+
+function PLUGIN:DataLoad( )
+	self:LoadTexts( )
+end
+
+function PLUGIN:DataSave( )
+	self:SaveTexts( )
+end
+
+function PLUGIN:AddText( pl, text, size )
+	local tr = pl:GetEyeTraceNoCursor( )
+	local data = {
+		index = #self.textLists + 1,
+		pos = tr.HitPos + tr.HitNormal,
+		ang = tr.HitNormal:Angle( ),
+		text = text,
+		size = math.max( math.abs( size or 0.25 ), 0.005 )
+	}
+	
+	data.ang:RotateAroundAxis( data.ang:Up( ), 90 )
+	data.ang:RotateAroundAxis( data.ang:Forward( ), 90 )
+
+	self.textLists[ data.index ] = data
+	
+	self:SyncTextAll( )
+	self:SaveTexts( )
+end
+
+function PLUGIN:RemoveText( pos, range )
+	range = tonumber( range )
+	local count = 0
+	for k, v in pairs( self.textLists ) do
+		if ( v.pos:Distance( pos ) <= range ) then
+			netstream.Start( nil, "catherine.plugin.goodtext.RemoveText", v.index )
+			table.remove( self.textLists, k )
+			count = count + 1
+		end
+	end
+	
+	self:SaveTexts( )
+	
+	return count
+end
+
+function PLUGIN:SyncTextAll( pl )
+	if ( !pl ) then pl = nil end
+	for k, v in pairs( self.textLists ) do
+		netstream.Start( pl, "catherine.plugin.goodtext.SyncText", { index = k, text = v.text, pos = v.pos, ang = v.ang, size = v.size } )
+	end
+end
