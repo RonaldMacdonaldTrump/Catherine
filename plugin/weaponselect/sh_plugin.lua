4cf4c6f7cba6843c6056b611eb438962820a11e7
L7D <smhjyh2009@gmail.com>
2015-05-20 12:44:53 +0900

    Weaponselect plugin optimize.

--- plugin/weaponselect/sh_plugin.lua
+++ plugin/weaponselect/sh_plugin.lua
@@ -33,7 +33,7 @@ catherine.language.Merge( "korean", {
 
 if ( SERVER ) then
 	concommand.Add( "cat_ws_selectWeapon", function( pl, _, args )
-		pl.SelectWeapon( pl, args[ 1 ] )
+		pl:SelectWeapon( args[ 1 ] )
 	end )
 else
 	PLUGIN.latestSlot = PLUGIN.latestSlot or 1
@@ -42,13 +42,13 @@ else
 	PLUGIN.markup = PLUGIN.markup or nil
 	
 	function PLUGIN:PlayerBindPress( pl, bind, pressed )
-		local wep = pl.GetActiveWeapon( pl )
-		local weps = pl.GetWeapons( pl )
-		if ( pl.InVehicle( pl ) or ( IsValid( wep ) and wep.GetClass( wep ) == "weapon_physgun" and pl.KeyDown( pl, IN_ATTACK ) ) ) then return end
+		local wep = pl:GetActiveWeapon( )
+		if ( pl:InVehicle( ) or ( IsValid( wep ) and wep:GetClass( ) == "weapon_physgun" and pl:KeyDown( IN_ATTACK ) ) ) then return end
+		local weps = pl:GetWeapons( )
 		
-		bind = bind.lower( bind )
+		bind = bind:lower( )
 		
-		if ( bind.find( bind, "invnext" ) and pressed ) then
+		if ( bind:find( "invnext" ) and pressed ) then
 			self.latestSlot = self.latestSlot + 1
 			
 			if ( self.latestSlot > #weps ) then
@@ -58,7 +58,7 @@ else
 			hook.Run( "OnWeaponSlotChanged", pl, self.latestSlot )
 			
 			return true
-		elseif ( bind.find( bind, "invprev" ) and pressed ) then
+		elseif ( bind:find( "invprev" ) and pressed ) then
 			self.latestSlot = self.latestSlot - 1
 			
 			if ( self.latestSlot <= 0 ) then
@@ -68,7 +68,7 @@ else
 			hook.Run( "OnWeaponSlotChanged", pl, self.latestSlot )
 			
 			return true
-		elseif ( bind.find( bind, "+attack" ) and pressed ) then
+		elseif ( bind:find( "+attack" ) and pressed ) then
 			if ( self.noShowTime > CurTime( ) ) then
 				self.showTime = 0
 				self.noShowTime = 0
@@ -76,14 +76,15 @@ else
 				for k, v in pairs( weps ) do
 					if ( k != self.latestSlot ) then continue end
 					
-					RunConsoleCommand( "cat_ws_selectWeapon", v.GetClass( v ) )
+					RunConsoleCommand( "cat_ws_selectWeapon", v:GetClass( ) )
 					
 					return true
 				end
 			end
-		elseif ( bind.find( bind, "slot" ) ) then
-			self.latestSlot = math.Clamp( tonumber( bind.match( bind, "slot(%d)" ) ) or 1, 1, #weps )
-			self.showTime = CurTime( ) + 4 self.noShowTime = CurTime( ) + 5
+		elseif ( bind:find( "slot" ) ) then
+			self.latestSlot = math.Clamp( tonumber( bind:match( "slot(%d)" ) ) or 1, 1, #weps )
+			self.showTime = CurTime( ) + 4
+			self.noShowTime = CurTime( ) + 5
 			
 			return true
 		end
@@ -93,10 +94,10 @@ else
 		self.showTime = CurTime( ) + 4
 		self.noShowTime = CurTime( ) + 5
 		
-		for k, v in pairs( pl.GetWeapons( pl ) ) do
+		for k, v in pairs( pl:GetWeapons( ) ) do
 			if ( k != self.latestSlot ) then continue end
 			
-			if ( v.Instructions and v.Instructions.find( v.Instructions, "%S" ) ) then
+			if ( v.Instructions and v.Instructions:find( "%S" ) ) then
 				self.markup = markup.Parse( "<font=catherine_outline15>" .. v.Instructions .. "</font>" )
 				
 				return
@@ -109,7 +110,7 @@ else
 	function PLUGIN:HUDDraw( )
 		local defx, defy = ScrW( ) * 0.175, ScrH( ) * 0.4
 		
-		for k, v in pairs( LocalPlayer( ).GetWeapons( LocalPlayer( ) ) ) do
+		for k, v in pairs( LocalPlayer( ):GetWeapons( ) ) do
 			local col = Color( 255, 255, 255 )
 			
 			col.a = math.Clamp( 255 - math.TimeFraction( self.showTime, self.noShowTime, CurTime( ) ) * 255, 0, 255 )
@@ -118,7 +119,7 @@ else
 				draw.SimpleText( ">", "catherine_normal25", defx - 20, defy + ( k * 30 ), col, TEXT_ALIGN_LEFT, 1 )
 			end
 			
-			draw.SimpleText( v.GetPrintName( v ), "catherine_normal25", defx, defy + ( k * 30 ), col, TEXT_ALIGN_LEFT, 1 )
+			draw.SimpleText( v:GetPrintName( ), "catherine_normal25", defx, defy + ( k * 30 ), col, TEXT_ALIGN_LEFT, 1 )
 			
 			if ( k == self.latestSlot and self.markup ) then
 				self.markup:Draw( defx + 128, defy + 24, 0, 1, col.a )
