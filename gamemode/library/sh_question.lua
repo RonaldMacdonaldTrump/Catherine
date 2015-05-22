bfd7a9fe165483f28ca50c3dda7c47eac7b76af2
L7D <smhjyh2009@gmail.com>
2015-05-22 15:04:24 +0900

    Question library 15%.

--- gamemode/library/sh_question.lua
+++ gamemode/library/sh_question.lua
@@ -17,22 +17,75 @@ along with Catherine.  If not, see <http://www.gnu.org/licenses/>.
 ]]--
 
 catherine.question = catherine.question or { }
-catherine.question.lists = { }
+catherine.question.lists = { multipleChoice = { }, descriptive = { } }
+CAT_QUESTION_MULTIPLE_CHOICE = 1
+CAT_QUESTION_DESCRIPTIVE = 2
 
-function catherine.question.Register( )
-	
+function catherine.question.RegisterMultipleChoice( title, answerList, answerIndex )
+	catherine.question.lists.multipleChoice[ #catherine.question.lists.multipleChoice + 1 ] = {
+		title = title,
+		answerList = answerList,
+		answerIndex = answerIndex
+	}
 end
 
-function catherine.question.GetAll( )
-	return catherine.question.lists
+function catherine.question.RegisterDescriptive( title, defVal )
+	catherine.question.lists.descriptive[ #catherine.question.lists.descriptive + 1 ] = {
+		title = title,
+		defVal = defVal
+	}
 end
 
-function catherine.question.FindByIndex( index )
-	return catherine.question.lists[ index ]
+function catherine.question.GetAllMultipleChoice( )
+	return catherine.question.lists.multipleChoice
+end
+
+function catherine.question.GetAllDescriptive( )
+	return catherine.question.lists.descriptive
+end
+
+function catherine.question.FindByIndex( typ, index )
+	if ( typ == CAT_QUESTION_MULTIPLE_CHOICE ) then
+		return catherine.question.lists.multipleChoice[ index ]
+	elseif ( typ == CAT_QUESTION_DESCRIPTIVE ) then
+		return catherine.question.lists.descriptive[ index ]
+	end
 end
 
 if ( SERVER ) then
-
+	function catherine.question.InitializeData( pl )
+		local data = {
+			questions = { }
+		}
+		
+		local questionType = catherine.question.GetQuestionType( )
+		
+		if ( questionType == CAT_QUESTION_MULTIPLE_CHOICE ) then
+			local questions = catherine.question.GetAllMultipleChoice( )
+			
+			if ( #questions > 0 ) then
+				data.questions = questions
+			else
+				print("Can't run - reason 1")
+			end
+		elseif ( questionType == CAT_QUESTION_DESCRIPTIVE ) then
+			local questions = catherine.question.GetAllDescriptive( )
+			
+			if ( #questions > 0 ) then
+				data.questions = questions
+			else
+				print("Can't run - reason 2")
+			end
+		end
+	end
+	
+	function catherine.question.Check( pl, data )
+		
+	end
+	
+	function catherine.question.GetQuestionType( )
+		return #catherine.util.GetAdmins( ) > 0 and CAT_QUESTION_DESCRIPTIVE or CAT_QUESTION_MULTIPLE_CHOICE
+	end
 else
 
 end
