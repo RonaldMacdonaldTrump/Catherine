94f1e44362ad99d6e375769c9134d7b9a2a7e22b
L7D <smhjyh2009@gmail.com>
2015-05-22 14:27:25 +0900

    Question library.

---
+++ gamemode/derma/cl_question.lua
@@ -0,0 +1,47 @@
+--[[
+< CATHERINE > - A free role-playing framework for Garry's Mod.
+Development and design by L7D.
+
+Catherine is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Catherine.  If not, see <http://www.gnu.org/licenses/>.
+]]--
+
+local PANEL = { }
+
+function PANEL:Init( )
+	catherine.vgui.question = self
+
+	self.player = LocalPlayer( )
+	self.w, self.h = ScrW( ), ScrH( )
+
+	self:SetSize( self.w, self.h )
+	self:Center( )
+	self:SetTitle( "QUESTION" )
+	self:MakePopup( )
+	self:ShowCloseButton( true )
+	
+end
+
+function PANEL:Paint( w, h )
+
+end
+
+function PANEL:Close( )
+	if ( self.closeing ) then return end
+	
+	self.closeing = true
+	self:Remove( )
+	self = nil
+end
+
+vgui.Register( "catherine.vgui.question", PANEL, "DFrame" )
