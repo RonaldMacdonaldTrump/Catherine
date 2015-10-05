081e2b18b70530c39cfb121f836bfdbf7ce6f674
L7D <smhjyh2009@gmail.com>
2015-10-05 15:36:33 +0900

    Code optimizer.

--- framework/library/cl_hud.lua
+++ framework/library/cl_hud.lua
@@ -16,10 +16,6 @@ You should have received a copy of the GNU General Public License
 along with Catherine.  If not, see <http://www.gnu.org/licenses/>.
 ]]--
 
-if ( !catherine.option ) then
-	catherine.util.Include( "cl_option.lua" )
-end
-
 catherine.hud = catherine.hud or {
 	welcomeIntroWorkingData = nil,
 	welcomeIntroAnimations = { },
